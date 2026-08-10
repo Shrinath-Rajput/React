780d5ceeedebf727f5585a07e4d95a7d15f8cc7a
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-08-10 10:01:00 +0530

    all

--- React-State-Project/src/counter.jsx
+++ React-State-Project/src/counter.jsx
@@ -9,12 +9,16 @@ function Counter(){
         setCount(count + 1);
     }
     
+function Decrement(){
+        setCount(count - 1);
+    }
     
     return(
         <div className="counter">
             <h1>Counter</h1>
             <h2>{count}</h2>
             <button onClick={Increment}>Increment</button>
+            <button onClick={Decrement}>Decrement</button>
         </div>
     )
 }
